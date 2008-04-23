10be1c128d99ffdbb0b5e7438b4fdf2444be4104
Craig Buchek <craig@boochtek.com>
2008-04-22 19:21:46 -0500

    Add someacceptance tests (not working yet).

---
+++ spec/acceptance/user_management.rb
@@ -0,0 +1,8 @@
+
+describe 'Admin adds a user' do
+  # NOTE: Email address is required. Username and password will be emailed.
+end
+
+
+describe 'Admin resets password for a user' do
+end
---
+++ spec/acceptance/user_registration.rb
@@ -0,0 +1,4 @@
+
+describe 'User registers for an account' do
+  # NOTE: Email address is required. Username and password will be emailed.
+end
